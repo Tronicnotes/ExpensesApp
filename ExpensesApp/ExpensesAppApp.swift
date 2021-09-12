89e9127592e649b86f41ab1b507a8e89e005bdba
Kurt Mohring <kurt.mohring@vacasa.com>
2021-09-12 13:31:29 +1200

    Initialise Xcode project

---
+++ ExpensesApp/ExpensesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesAppApp.swift
+//  ExpensesApp
+//
+//  Created by Kurt Mohring on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
